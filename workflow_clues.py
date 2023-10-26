5fb32392dbe975f308510be8f2ec3b28df1fded5
erik <qulli.erik@gmail.com>
2023-10-26 11:32:37 +0200

    Tree traversels and Relate

--- workflow_clues.py
+++ workflow_clues.py
@@ -3,42 +3,44 @@ import os
 from groups import Group
 os.environ['NUMEXPR_MAX_THREADS'] = '8' # to suppress a warning
 
-gwf = Workflow()
+gwf = Workflow(defaults={"account": "baboondiversity"})
 
 # Absolute paths to various parts - possibly rewrite to use relative paths
-path_to_relate = "/faststorage/home/eriks/relate-clues/relate_v1.1.2_x86_64_dynamic/"
-path_to_prepared = "/faststorage/home/eriks/relate-clues/steps/"
+path_to_relate = "/home/eriks/baboondiversity/people/eriks/second_analysis_baboons/relate/relate_v1.1.7_x86_64_dynamic/"
 path_to_script = "/faststorage/home/eriks/relate-clues/scripts/clues_master.py"
-path_to_results = "/faststorage/home/eriks/relate-clues/results/"
-path_to_relate_results = "/faststorage/home/eriks/relate-clues/results/"
+path_to_results = "/home/eriks/baboondiversity/people/eriks/second_analysis_baboons/results/"
+path_to_relate_results = "/home/eriks/baboondiversity/people/eriks/second_analysis_baboons/steps/{}_relate/"
 summarize_script = "/faststorage/home/eriks/relate-clues/scripts/summarize_clues.py"
 table_gen_script = "/faststorage/home/eriks/relate-clues/scripts/table_generation.py"
-# Name of directory, followed by mutation rate and populaiton size.
-pop_information = {"CEU_kept_prepared/": ["1.25e-8", "30000"],
-                   } # "YRI_kept_prepared/": ["1.25e-8", "32000"]
+# Mutation rate and population size
+pop_information = ["0.57e-8", "50000"]
+pop_information_x = ["0.46e-8", "25000"]
 # specification of clues variables. Timebins indicates the time to test for selection
 #  burn_in and thinning are used for MCMC sampling
-timebins = "/faststorage/home/eriks/relate-clues/data/clues_supporting/time1500_2500.txt"
+# timebins = "/faststorage/home/eriks/relate-clues/data/clues_supporting/time0_500.txt"
 relate_mcmc = 150
 burn_in = 50
 thinning = 1
-daf_bound = 0.25
-prioritize_sites = False
+daf_bound = "0.10_0.90"
 # Chromosome number, start and end of window, as well as number of tests in total and number of tests per job.
-chromosome = "2"
-window_start = 136000000
-window_end = 137000000
-number_of_tests = 100
-tests_per_job = 10
+run_combinations = [{"pop_name": "Tanzania_Olive", "chromosome": ["1", "2"],
+                     "window_start": 0, "window_end": "max",
+                     "number_of_tests": 100, "tests_per_job": 25,
+                     "doublestack": True, "run_name": "tanz_aut_test"}]
+# chromosome = "hapX"
+# window_start = 0
+# window_end = 144000000
+# number_of_tests = 100
+# tests_per_job = 10
 # Name added to dir
-identifier = "no_prio_ooa"
-dir_suffix = "chrom{}_{}_{}_{}_{}".format(chromosome, window_start, window_end, number_of_tests, identifier)
+# identifier = "prio"
+# dir_suffix = "chrom{}_{}_{}_{}_{}".format(chromosome, window_start, window_end, number_of_tests, identifier)
 
 
-def table_gen(script, relate_results, start, end, spacing, daf_bound, prioritize, out_dir):
+def table_gen(run_name, script, relate_results, start, end, number_of_tests, daf_bound, doublestack, out_dir, chrom):
     i = relate_results+".freq"
     inputs = [i]
-    outputs = out_dir+"clues_table_temp.txt"
+    outputs = out_dir+"{}_clues_table_start_{}.txt".format(chrom, run_name)
     options = {
         "cores": 2,
         "memory": "10g",
@@ -46,30 +48,32 @@ def table_gen(script, relate_results, start, end, spacing, daf_bound, prioritize
     }
     spec = """
     python {} {} {} {} {} {} {} -o {}
-    """.format(script, relate_results, start, end, int(spacing), daf_bound, prioritize, out_dir)
-    print(spec)
+    """.format(script, relate_results, start, end, int(number_of_tests), daf_bound, doublestack, outputs)
+    print(outputs)
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
 
-def relate_clues(chunk, chunk_number, script, number, out_dir, pop_inf, input_dir, relate_path, timebins, mcmc, burnin, thin):
+def relate_clues(chunk, chunk_number, run_name, script, number, out_dir, pop_inf, input_dir, relate_path, mcmc, burnin, inputs):
     i = input_dir+"chrom{}_popsize.coal".format(number)
-    inputs = [out_dir+"clues_table_temp.txt"]
-    outputs = out_dir+"chunk{}_table.txt".format(chunk)
-    walltime = 10*(tests_per_job//10)+10
+    chrom = "chrom{}".format(number)
+    inputs = inputs
+    out_name = "snp_results/{}_chunk{}_{}_table.txt".format(chrom, chunk, run_name)
+    outputs = out_dir+out_name
+    walltime = 2*(tests_per_job//5)+2
     options = {
-        "cores": 10,
-        "memory": "30g",
+        "cores": 5,
+        "memory": "40g",
         "walltime": "{}:00:00".format(walltime)
     }
     spec = """
-    cd clues/
-    python {} {} {} -i {} -o {} -m {} -r {} -b {} --mcmc {} --burnin {} --thin {}
-    """.format(script, chunk, chunk_number, i[:-5], out_dir, pop_inf[0], relate_path, timebins, mcmc, burnin, thin)
+    cd /home/eriks/faststorage/relate-clues/clues/
+    python {} {} {} -i {} -o {} -n {} -m {} -r {} -t {} -c {} --mcmc {} --burnin {}
+    """.format(script, chunk, chunk_number, i[:-5], out_dir, out_name, pop_inf[0], relate_path, inputs, chrom, mcmc, burnin)
     print(spec)
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
 
-def summarize_clues(clues_results, script, out_dir):
+def summarize_clues(clues_results, script, out_dir, table):
     inputs = clues_results
     outputs = out_dir+"clues_table.txt"
     options = {
@@ -78,34 +82,48 @@ def summarize_clues(clues_results, script, out_dir):
         "walltime": "1:00:00"
     }
     spec = """
-    python {} -i {} -o {}
-    """.format(script, out_dir, outputs)
-    print(spec)
+    python {} -i {} -t {} -o {}
+    """.format(script, ",".join(inputs), table, outputs)
+    print(inputs, spec)
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
 
-site_spacing = (window_end-window_start)/number_of_tests
-chunk_number = number_of_tests//tests_per_job + (number_of_tests % tests_per_job > 0)
-chunk_list = list(range(1, chunk_number+1))
-print("""Trying to place a site every {} bases, leading to {} tests.
-      """.format(site_spacing, number_of_tests))
-for pop in pop_information:
-    pop_name = pop[:-10]
-    out_dir = os.getcwd()+"/results/{}_{}_clues/".format(pop_name, dir_suffix)
-    os.makedirs(out_dir+"tmp/", exist_ok=True)
-    relate_results_path = path_to_relate_results+"{}_relate/chrom{}_selection".format(pop_name, chromosome)
-    with Group(gwf, suffix=pop_name+"_"+identifier) as g:
-        g.target_from_template("temp_table",
-                               table_gen(table_gen_script, relate_results_path, window_start,
-                                         window_end, site_spacing, daf_bound, prioritize_sites, out_dir))
-        total_workflow = g.map(relate_clues, chunk_list,
-                               name="chrom_{}_{}_{}".format(chromosome, window_start, number_of_tests),
-                               extra={"chunk_number": chunk_number, "script": path_to_script,
-                                      "number": chromosome, "out_dir": out_dir,
-                                      "pop_inf": pop_information[pop], "input_dir": path_to_prepared+"{}_relate/".format(pop_name),
-                                      "relate_path": path_to_relate, "timebins": timebins,
-                                      "mcmc": relate_mcmc, "burnin": burn_in, "thin": thinning})
-        g.target_from_template("summarize",
-                               summarize_clues(total_workflow.outputs, script=summarize_script,
-                                               out_dir=out_dir))
+# Workflow in which the targets are defined by the table_gen workflow.
 
+for run in run_combinations:
+    for chromosome in run["chromosome"]:
+        pop_name = run["pop_name"]
+        window_start = run["window_start"]
+        window_end = run["window_end"]
+        number_of_tests, tests_per_job = run["number_of_tests"], run["tests_per_job"]
+        doublestack = run["doublestack"]
+        run_name = run["run_name"]
+        chunk_number = number_of_tests//tests_per_job + (number_of_tests % tests_per_job > 0)
+        chunk_list = list(range(1, chunk_number+1))
+        out_dir = os.getcwd()+"/results/{}_clues/".format(pop_name)
+        if chromosome in ["hapX", "X"]:
+            pop_inf = pop_information_x
+        else:
+            pop_inf = pop_information
+        print(out_dir)
+        os.makedirs(out_dir+"snp_results/", exist_ok=True)
+        relate_results_path = "results/{}_relate/chrom{}_selection".format(pop_name, chromosome)
+        with Group(gwf, suffix="{}".format(pop_name)) as g:
+            table_clues = g.target_from_template("table_picks_{}_{}".format(chromosome, run_name),
+                                                 table_gen(run_name, table_gen_script, relate_results_path, window_start,
+                                                           window_end, number_of_tests, daf_bound, doublestack, out_dir,
+                                                           chromosome))
+            total_workflow = g.map(relate_clues, chunk_list,
+                               name="chrom_{}_{}".format(chromosome, run_name),
+                               extra={"run_name": run_name, "chunk_number": chunk_number, "script": path_to_script,
+                                      "number": chromosome, "out_dir": out_dir, "pop_inf": pop_inf,
+                                      "input_dir": path_to_relate_results.format(pop_name),
+                                      "relate_path": path_to_relate,
+                                      "mcmc": relate_mcmc, "burnin": burn_in,
+                                      "inputs": table_clues.outputs})
+            g.target_from_template("summarize_{}_{}".format(chromosome, run_name),
+                               summarize_clues(total_workflow.outputs, script=summarize_script,
+                                               out_dir=out_dir+"{}_{}".format(chromosome, run_name), table=table_clues.outputs))
+
+
+# Alternate version with a custom table defined by hand.
--- workflow_single_population_relate.py
+++ workflow_single_population_relate.py
@@ -18,14 +18,23 @@ path_to_poplabels_hapx = "data/pops/haploid_x_8cluster.sample" # Haploid X conve
 path_to_mask = "/home/eriks/baboondiversity/data/callability_panu3_26_04_2021/panu3.npmask.chr{}.fa"
 genetic_map = "/home/eriks/baboondiversity/data/PG_panu3_recombination_map/mikumi_pyrho_genetic_map_chr{}.txt"
 haps_sample_dir = "steps/haps_sample/"
-
 pop_files = "data/pops/"
-keep_pop_l_l =[["Ethiopian Olive"]] # [["All_Samples"]] #[["Ethiopian Olive"], ["All_Samples"]]
+
+meta_data_samples = pd.read_csv("data/Papio_metadata_with_clustering_sci.txt", sep =" ")
+eastern_yellow_IDs = meta_data_samples.loc[meta_data_samples.C_origin == "Eastern Yellow, Tanzania"].PGDP_ID
+etholive_IDs = meta_data_samples.loc[meta_data_samples.C_origin == "Anubis, Ethiopia"].PGDP_ID
+tanzolive_IDs = meta_data_samples.loc[meta_data_samples.C_origin == "Anubis, Tanzania"].PGDP_ID
+
+
+
+keep_pop_l_l =[#["Ethiopian_Olive", etholive_IDs], #["Eastern_Yellow", eastern_yellow_IDs],
+               ["All_Samples", meta_data_samples.PGDP_ID], ["Tanzania_Olive", tanzolive_IDs]]
+#keep_pop_l_l =[["Tanzania_Olive", tanzolive_IDs], ["All_Samples"]] #[["Ethiopian_Olive"], ["All_Samples"]]
 pop_information = ["0.57e-8", "50000"]
 pop_information_x = ["0.46e-8", "25000"]
 
 vcfs = []
-autosomes = list(range(20, 21))
+autosomes = list(range(1, 21))
 chromosomes = autosomes + ["X"]
 
 for chrom in autosomes:
@@ -53,7 +62,7 @@ def vcf_to_haps(vcf_path, chrom, run_name, relate_path, output_dir):
     outputs = {"haps": haps_out, "sample": sample_out}
     options = {
         "cores": 2,
-        "memory": "4g",
+        "memory": "15g",
         "walltime": "1:00:00",
         "account": "baboondiversity"
     }
@@ -72,13 +81,17 @@ def prepare_input(haps, sample, mask, ancestor, pop_l, output_dir, poplabels, re
     remove_ids = ""
     pop_file = pd.read_csv(poplabels, sep=" ")
     if pop_l != ["All_Samples"]:
-        ids_to_remove = pop_file.loc[~pop_file["POP"].isin(pop_l)]
+        ids_to_remove = pop_file.loc[~pop_file["ID"].isin(pop_l[1])]
         ids_to_remove.ID.to_csv(output_dir+"IDs_removed{}.txt".format(number),index=False, header=False)
         remove_ids = " --remove_ids "+output_dir+"IDs_removed{}.txt".format(number)
-        labels_to_keep = pop_file.loc[pop_file["POP"].isin(pop_l)]
+        labels_to_keep = pop_file.loc[pop_file["ID"].isin(pop_l[1])]
         labels_to_keep.to_csv(destination_name+".poplabels", index=False, sep=" ")
     else:
-        pop_file.to_csv(destination_name+".poplabels", index=False, sep=" ")
+        ids_to_remove = pop_file.loc[pop_file["POP"].isin(["Gelada"])]
+        ids_to_remove.ID.to_csv(output_dir+"IDs_removed{}.txt".format(number),index=False, header=False)
+        remove_ids = " --remove_ids "+output_dir+"IDs_removed{}.txt".format(number)
+        labels_to_keep = pop_file.loc[~pop_file["POP"].isin(["Gelada"])]
+        labels_to_keep.to_csv(destination_name+".poplabels", index=False, sep=" ")
     if number == "hapX":
         number = "X"
     n_mask = mask.format(number)
@@ -86,13 +99,13 @@ def prepare_input(haps, sample, mask, ancestor, pop_l, output_dir, poplabels, re
     outputs = {"haps": destination_name+".haps.gz", "sample": destination_name+".sample.gz"}
     options = {
         "cores": 2,
-        "memory": "4g",
+        "memory": "15g",
         "walltime": "4:00:00",
         "account": "baboondiversity"
     }
     spec = """
-    {}  --haps {} --sample {} --mask {} --ancestor {}{} -o {}
-    """.format(PrepareInputFiles, haps, sample, n_mask, ancestor_in, remove_ids, destination_name)
+    {}  --haps {} --sample {} --mask {} --ancestor {}{} --poplabels {} -o {}
+    """.format(PrepareInputFiles, haps, sample, n_mask, ancestor_in, remove_ids, poplabels, destination_name)
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
 
@@ -100,7 +113,7 @@ def hapx_convert_prepare_input(vcf_path, chrom, run_name, relate_path, haps_samp
                                mask, ancestor, pop_l, output_dir, poplabels):
     """Converts vcf files to haps/sample using the script from Relate """
     inputs = [vcf_path+".vcf.gz"]
-    vcf_haps_suffix = ("_".join(pop_l)).replace(" ", "_")
+    vcf_haps_suffix = ("_".join(pop_l[0])).replace(" ", "_")
     output_vcf = haps_sample_dir + "chromhapX_{}".format(vcf_haps_suffix)
     haps_out = os.path.join(haps_sample_dir, "chrom{}.haps". format(vcf_haps_suffix))
     sample_out = os.path.join(haps_sample_dir, "chrom{}.sample".format(vcf_haps_suffix))
@@ -108,10 +121,11 @@ def hapx_convert_prepare_input(vcf_path, chrom, run_name, relate_path, haps_samp
     PrepareInputFiles = relate_path + "scripts/PrepareInputFiles/PrepareInputFiles.sh"
     destination_name = output_dir + "chrom{}".format("hapX")
     pop_file = pd.read_csv(poplabels, sep=" ")
+    kept_IDs = pd.concat([pop_l[1], pop_l[1]+"_a", pop_l[1]+"_b"])
     if pop_l != ["All_Samples"]:
-        ids_to_remove = pop_file.loc[~pop_file["POP"].isin(pop_l)]
-        ids_to_remove.ID.to_csv(output_dir+"IDs_removed{}.txt".format("hapX"),index=False, header=False)
-        labels_to_keep = pop_file.loc[pop_file["POP"].isin(pop_l)]
+        ids_to_remove = pop_file.loc[~pop_file["ID"].isin(kept_IDs)]
+        ids_to_remove.ID.to_csv(output_dir+"IDs_removed{}.txt".format("hapX"), index=False, header=False)
+        labels_to_keep = pop_file.loc[pop_file["ID"].isin(kept_IDs)]
         labels_to_keep.to_csv(destination_name+".poplabels", index=False, sep=" ")
         sample_list = ",".join(labels_to_keep.ID)
     else:
@@ -140,7 +154,7 @@ def full_relate(number, genetic_map, out_dir, pop_inf, prep_dir, base_path, rela
     sample = prep_dir+"chrom{}.sample.gz".format(number)
     annot = prep_dir+"chrom{}.annot".format(number)
     dist = prep_dir+"chrom{}.dist.gz".format(number)
-    if number == "X" or "hapX":
+    if number == "X" or number == "hapX":
         m = pop_information_x[0]
         n = pop_information_x[1]
     else:
@@ -153,12 +167,12 @@ def full_relate(number, genetic_map, out_dir, pop_inf, prep_dir, base_path, rela
     options = {
         "cores": 4,
         "memory": "40g",
-        "walltime": "12:00:00",
+        "walltime": "24:00:00",
         "account": "baboondiversity"
     }
     spec = """
     cd {}
-    {} --mode All -m {} -N {} --haps {} --sample {} --map {} -o {} --annot {} --dist {} --memory 20
+    {} --mode All -m {} -N {} --haps {} --sample {} --map {} -o {} --annot {} --dist {} --memory 30
     """.format(out_dir,
                Relate, m, n, haps, sample, genetic_map, o, annot,  dist)
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
@@ -176,37 +190,40 @@ def estimate_pop_size(number, out_dir, pop_inf, prep_dir, relate_path):
     Relate = os.path.join(relate_path, "scripts/EstimatePopulationSize/EstimatePopulationSize.sh")
     poplabels = prep_dir+"chrom{}.poplabels".format(number)
     options = {
-        "cores": 14,
-        "memory": "60g",
-        "walltime": "12:00:00",
+        "cores": 7,
+        "memory": "100g",
+        "walltime": "36:00:00",
         "account": "baboondiversity"
     }
     spec = """
     {} -i {} -m {} --poplabels {} -o {} --threshold 0 --num_iter 5 --years_per_gen 11 --threads 14
     """.format(Relate, i[:-4], m, poplabels, o)
-    print(spec)
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
 
 def detect_selection(number, in_dir, out_results, pop_inf, prep_dir, relate_path):
     i = in_dir+"chrom{}_popsize.coal".format(number)
     o = out_results+"chrom{}_selection".format(number)
-    if number == "X" or "hapX":
+    o2 = out_results+"chrom{}".format(number)
+    if number == "X" or number == "hapX":
         m = pop_information_x[0]
     else:
         m = pop_inf[0]
     inputs = [i]
-    outputs = [o+".freq"]
+    outputs = [o+".freq", o2+".trees"]
     Relate = os.path.join(relate_path, "scripts/DetectSelection/DetectSelection.sh")
+    relate_to_tskit = os.path.join(relate_path, "bin/RelateFileFormats")
     options = {
-        "cores": 5,
-        "memory": "20g",
-        "walltime": "1:00:00",
+        "cores": 8,
+        "memory": "80g",
+        "walltime": "12:00:00",
         "account": "baboondiversity"
     }
     spec = """
-    {} -i {} -m {} -o {}
-    """.format(Relate, i[:-5], m, o)
+    {} -i {} -m {} -o {} --years_per_gen 11
+    {} --mode ConvertToTreeSequence -i {} -o {}
+    """.format(Relate, i[:-5], m, o,
+               relate_to_tskit, i[:-5], o2)
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
 
@@ -217,10 +234,11 @@ os.makedirs(haps_sample_dir, exist_ok=True)
 haps_sample_aut = gwf.map(vcf_to_haps, vcfs, extra={"relate_path": path_to_relate, "output_dir": haps_sample_dir})
 haps_sample_x = gwf.map(vcf_to_haps, x_vcf, extra={"relate_path": path_to_relate, "output_dir": haps_sample_dir},
                         name='x_to_haps')
+# haps_sample_hapx = gwf.map(vcf_to_haps, hapx_vcf, extra={"relate_path": path_to_relate, "output_dir": haps_sample_dir},
+#                         name='hapx_to_haps')
 
 for pop_l in keep_pop_l_l:
-    popname = "_".join([x.replace(" ", "_") for x in pop_l])
-    print(popname)
+    popname = pop_l[0].replace(" ", "_")
     path_to_prep = os.getcwd()+"/steps/relate_{}_prepared/".format(popname)
     os.makedirs(path_to_prep, exist_ok=True)
     os.makedirs(path_to_prep+"temp/", exist_ok=True)
@@ -233,6 +251,10 @@ for pop_l in keep_pop_l_l:
                         "mask": path_to_mask, "ancestor": path_to_ancestor, "pop_l": pop_l,
                         "output_dir": path_to_prep, "poplabels": path_to_poplabels_females,
                         "relate_path": path_to_relate})
+        # preps_hapx = g.map(prepare_input, haps_sample_hapx.outputs, name="prepare_input_hapx", extra={
+        #                 "mask": path_to_mask, "ancestor": path_to_ancestor, "pop_l": pop_l,
+        #                 "output_dir": path_to_prep, "poplabels": path_to_poplabels_hapx,
+        #                 "relate_path": path_to_relate})
         g.map(hapx_convert_prepare_input, hapx_vcf, name="convert_prepare_hapx", extra={
                         "relate_path": path_to_relate, "haps_sample_dir": haps_sample_dir,
                         "mask": path_to_mask, "ancestor": path_to_ancestor, "pop_l": pop_l,
@@ -241,16 +263,16 @@ for pop_l in keep_pop_l_l:
     out_results = "results/{}_relate/".format(popname)
     os.makedirs(out_dir, exist_ok=True)
     os.makedirs(out_results, exist_ok=True)
-    # with Group(gwf, suffix=popname) as g:
-    #     relate = g.map(full_relate, l_d, name="relate", extra={
-    #         "out_dir": out_dir, "pop_inf": pop_information,
-    #         "prep_dir": path_to_prep, "base_path": os.getcwd(), "relate_path": path_to_relate
-    #     })
-    #     popsize = g.map(estimate_pop_size, chromosomes, name="popsize", extra={
-    #         "out_dir": out_dir, "pop_inf": pop_information,
-    #         "prep_dir": path_to_prep, "relate_path": path_to_relate
-    #     })
-    #     g.map(detect_selection, chromosomes, name="detect_selection", extra={
-    #         "in_dir": out_dir, "out_results": out_results, "pop_inf": pop_information,
-    #         "prep_dir": path_to_prep, "relate_path": path_to_relate
-    #     })
+    with Group(gwf, suffix=popname) as g:
+        relate = g.map(full_relate, l_d, name="relate", extra={
+            "out_dir": out_dir, "pop_inf": pop_information,
+            "prep_dir": path_to_prep, "base_path": os.getcwd(), "relate_path": path_to_relate
+        })
+        popsize = g.map(estimate_pop_size, chromosomes, name="popsize", extra={
+            "out_dir": out_dir, "pop_inf": pop_information,
+            "prep_dir": path_to_prep, "relate_path": path_to_relate
+        })
+        g.map(detect_selection, chromosomes, name="detect_selection", extra={
+            "in_dir": out_dir, "out_results": out_results, "pop_inf": pop_information,
+            "prep_dir": path_to_prep, "relate_path": path_to_relate
+        })
